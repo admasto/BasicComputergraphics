1d390a21f77c9d84e2f2abc309e141aec90508e3
admasto <adrianstoll@online.de>
2017-07-23 05:42:57 +0200

    Erste Struktur für CamController und Image Effects hinzugefüg Nachladen von Image Effect packet ohne Erfolg

---
+++ Assets/Scripts/CameraViewController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewController : MonoBehaviour {
+
+	public GreyValueCam greyValues;
+	public HeatmapCam heatmap;
+	public TemperaturemapCam temperaturemap;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("0")){
+
+		} 
+		if (Input.GetKeyUp("1")){
+			greyValues.OnRenderImage ();
+		} 
+		if (Input.GetKeyUp("2")) {
+			heatmap.OnRenderImage ();
+		}
+		if (Input.GetKeyUp ("3")) {
+			temperaturemap.OnRenderImage ();
+		}
+	}
+}
---
+++ Assets/Scripts/GreyValueImageEffect.cs
@@ -0,0 +1,45 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyValueCam : MonoBehaviour {
+
+	private RenderTexture sourceImage;
+	private RenderTexture diestinationImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnRenderImage (source, destination){
+
+	}
+}
+*/
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.ImageEffects
+{
+	[ExecuteInEditMode]
+	[AddComponentMenu("Image Effects/Color Adjustments/Grayscale")]
+	public class Grayscale : ImageEffectBase {
+		public Texture  textureRamp;
+
+		[Range(-1.0f,1.0f)]
+		public float    rampOffset;
+
+		// Called by camera to apply image effect
+		void OnRenderImage (RenderTexture source, RenderTexture destination) {
+			material.SetTexture("_RampTex", textureRamp);
+			material.SetFloat("_RampOffset", rampOffset);
+			Graphics.Blit (source, destination, material);
+		}
+	}
+}
