5b04e55e6dc7534e20b63a015b55597d9717287a
admasto <adrianstoll@online.de>
2017-07-23 06:21:30 +0200

    Erste Struktur für CamController und Image Effects hinzugefüg Nachladen von Image Effect packet ohne Erfolg

--- Assets/Scripts/CameraViewController.cs
+++ Assets/Scripts/CameraViewController.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class CameraViewController : MonoBehaviour {
 
-	public GreyValueCam greyValues = gameObject.GetComponent<GreyValueCam>();
-	public HeatmapCam heatmap = gameObject.GetComponent<HeatMapCam>();
-	public TemperaturemapCam temperaturemap = gameObject.GetComponent<TemperaturemapCam>();
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	GreyValueCam greyValues = gameObject.GetComponent<GreyValueCam>();
+	HeatmapCam heatmap = gameObject.GetComponent<HeatMapCam>();
+	TemperaturemapCam temperaturemap = gameObject.GetComponent<TemperaturemapCam>();
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp("0")){
+		if (Input.GetKeyUp ("0")) {
 
 		} 
-		if (Input.GetKeyUp("1")){
+		if (Input.GetKeyUp ("1")) {
 			greyValues.OnRenderImage ();
 		} 
-		if (Input.GetKeyUp("2")) {
+		if (Input.GetKeyUp ("2")) {
 			heatmap.OnRenderImage ();
 		}
 		if (Input.GetKeyUp ("3")) {
 			temperaturemap.OnRenderImage ();
 		}
+	}
 }
