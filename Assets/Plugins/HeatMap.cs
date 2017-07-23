9b308e93bef2e81b130a31d49542570ba285da74
admasto <adrianstoll@online.de>
2017-07-23 18:39:27 +0200

    Material HeatMapWalls, HeatMapTeapots usw erstellt. HeatMapshader hinzugefügt. Auf KeyUp '2' werden Materials ausgetauscht. Momentan OutOfBounds Exception in HeaterScript

--- Assets/Plugins/HeatMap.cs
+++ Assets/Plugins/HeatMap.cs
@@ -8,12 +8,9 @@ namespace UnityStandardAssets.ImageEffects
 	public class HeatMap : ImageEffectBase {
 		public Texture  textureRamp;
 
-		public float    rampOffset;
-
 		// Called by camera to apply image effect
 		void OnRenderImage (RenderTexture source, RenderTexture destination) {
 			material.SetTexture("_RampTex", textureRamp);
-			material.SetFloat("_RampOffset", rampOffset);
 			Graphics.Blit (source, destination, material);
 		}
 	}
--- Assets/Scripts/CameraViewController.cs
+++ Assets/Scripts/CameraViewController.cs
@@ -3,33 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.ImageEffects;
 
+
 public class CameraViewController : MonoBehaviour {
 	public RenderTexture source;
 	public RenderTexture destination;
+
+	public Material HeatMapGround;
+	public Material HeatMapCeiling;
+	public Material HeatMapWalls;
+	public Material HeatMapHeater;
+	public Material HeatMapTeapots;
+
 	private MonoBehaviour grayScale;
-	private MonoBehaviour heatMap;
+
+	public GameObject exampleTextureShader_Ceiling;
+	public GameObject Walls;
+	public GameObject Teapots;
+	public GameObject Heater;
+	public GameObject Ground;
+
 
 	void Start (){
 		grayScale = Camera.main.GetComponent<Grayscale> ();
 		grayScale.enabled = false;
 
-		heatMap = Camera.main.GetComponent<HeatMap> ();
-		heatMap.enabled = false;
+
+		//heatMap = Camera.main.GetComponent<HeatMap> ();
+		//heatMap.enabled = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp ("0")) {
 			grayScale.enabled = false;
-			heatMap.enabled = false;
+			//heatMap.enabled = false;
 		} 
 		if (Input.GetKeyUp ("1")) {
 			grayScale.enabled = true;
-			heatMap.enabled = false;
+			//heatMap.enabled = false;
 		} 
 		if (Input.GetKeyUp ("2")) {
-			heatMap.enabled = true;
 			grayScale.enabled = false;
-		}
+			Ground.GetComponent<Renderer>().material = HeatMapGround;
+			exampleTextureShader_Ceiling.GetComponent<Renderer>().material = HeatMapCeiling;
+			Walls.GetComponent<Renderer>().material = HeatMapWalls;
+			Heater.GetComponent<Renderer> ().material = HeatMapHeater;
+			Teapots.GetComponent<Renderer>().material = HeatMapTeapots;
+		}	
 	}
 }
